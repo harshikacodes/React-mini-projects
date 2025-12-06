8cb91376a8aabbc833d494ccfa85c5e4a6ce588d
harshika.malhotra <malhotra.harshikaa@gmail.com>
2025-12-06 20:02:37 +0530

    refactor Counter component layout and styling

--- CounterApp/src/Counter.jsx
+++ CounterApp/src/Counter.jsx
@@ -5,13 +5,17 @@ function Counter(){
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <h2>Counter App</h2>
-            <p>Count: {count}</p>
+        <div className="min-h-screen flex item-center justify-center bg-gray-100">
+            <div className="bg-white shadow-lg rounded-xl p-8 w-80 text-center">
+                <h2 className="text-2xl font-semibold mb-4">Counter App</h2>
+                <p className="text-xl font-bold mb-6">Count: {count}</p>
 
-            <MyButton text="+1" onClick={() => setCount(count+1)}/>
-            <MyButton text="-1" onClick={() => setCount(count-1)} disabled={count<=0}/>
-            <MyButton text="Reset" onClick={() => setCount(0)}/>
+                <div className="flex justify-center gap-3">
+                    <MyButton text="+1" onClick={() => setCount(count+1)}/>
+                    <MyButton text="-1" onClick={() => setCount(count-1)} disabled={count<=0}/>
+                    <MyButton text="Reset" onClick={() => setCount(0)}/>
+                </div>
+            </div>
         </div>
     );
 }
