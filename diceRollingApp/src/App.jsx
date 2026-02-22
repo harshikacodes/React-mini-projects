cac119118ab0308bae4564889f358420920bc2a2
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-22 21:00:57 +0530

    enhance UI layout

--- diceRollingApp/src/App.jsx
+++ diceRollingApp/src/App.jsx
@@ -22,10 +22,27 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1>Dice Roller</h1>
-      <img src={`./images/dice-${dice}.png`} alt="" />
-      <button onClick={rollDice}>Roll Dice</button>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white shadow-xl rounded-2xl p-8 text-center">
+        <h1 className="text-3xl font-bold mb-6">
+          Dice Roller
+        </h1>
+
+        <img 
+        src={`./images/dice-${dice}.png`} 
+        alt="dice"
+        className="w-32 mx-auto mb-6 transition-transform duration-200" 
+        />
+
+        <button 
+          onClick={rollDice}
+          disabled={isRolling}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-lg transition duration-200 disabled:bg-gray-200"
+        >
+          Roll Dice
+        </button>
+      </div>
+      
     </div>
   ) 
 }
