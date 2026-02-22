1eb8297f30f260738cfd950005861a8a51ffb853
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-02-22 20:47:10 +0530

    styling

--- diceRollingApp/src/App.jsx
+++ diceRollingApp/src/App.jsx
@@ -3,21 +3,26 @@ import { useState } from "react";
 function App() {
 
   const [dice, setDice] = useState(1);
+  const [isRolling, setIsRolling] = useState(false);
 
   function rollDice(){
+    if (isRolling) return;
+
+    setIsRolling(true);
+
     const interval = setInterval(() => {
       const random = Math.floor(Math.random() * 6) + 1;
       setDice(random);
-      console.log(random);
-    }, 50);
+    }, 10);
 
     setTimeout(() => {
       clearInterval(interval);
+      setIsRolling(false);
     }, 1000);
   }
 
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center">
       <h1>Dice Roller</h1>
       <img src={`./images/dice-${dice}.png`} alt="" />
       <button onClick={rollDice}>Roll Dice</button>
